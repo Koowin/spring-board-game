adbd65c24eef4e7d442d08cc2b75eff51d97a33a
Koowin <56014482+Koowin@users.noreply.github.com>
2022-06-22 08:58:52 +0900

    init: multi-player-chess-game

---
+++ multi-player/src/test/java/com/koowin/multiplayer/MultiPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koowin.multiplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
