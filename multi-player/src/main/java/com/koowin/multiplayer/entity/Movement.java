64310506985cbd2031154a356307c64c1d1d1561
Koowin <56014482+Koowin@users.noreply.github.com>
2022-07-04 23:16:35 +0900

    fix: movement

--- multi-player/src/main/java/com/koowin/multiplayer/entity/Movement.java
+++ multi-player/src/main/java/com/koowin/multiplayer/entity/Movement.java
@@ -1,5 +1,12 @@
 package com.koowin.multiplayer.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Movement {
 
 }
