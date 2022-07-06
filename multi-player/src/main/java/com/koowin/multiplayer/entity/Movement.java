05f9957ff88465649ab338ed45b7dfd6f10490eb
Koowin <56014482+Koowin@users.noreply.github.com>
2022-07-06 22:52:01 +0900

    feat: add DynamoDB

--- multi-player/src/main/java/com/koowin/multiplayer/entity/Movement.java
+++ multi-player/src/main/java/com/koowin/multiplayer/entity/Movement.java
@@ -1,5 +1,9 @@
 package com.koowin.multiplayer.entity;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +11,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@DynamoDBTable(tableName = "Movement")
 public class Movement {
+  @DynamoDBHashKey(attributeName = "gameId")
+  private Long gameId;
 
+  @DynamoDBAttribute
+  @DynamoDBIndexHashKey(attributeName = "turnCount")
+  private Long turnCount;
 }
